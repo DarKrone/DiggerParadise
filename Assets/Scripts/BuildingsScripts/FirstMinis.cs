b0ba68f5535f7e297f1232d4cef5562b8359c543
DarKrone <Brener-2000@mail.ru>
2024-07-09 17:40:34 +0400

    First buildings sprites

--- Assets/Scripts/BuildingsScripts/FirstMinis.cs
+++ Assets/Scripts/BuildingsScripts/FirstMinis.cs
@@ -1,14 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FirstMinis : MonoBehaviour, IComplitedConstruction
 {
-    [SerializeField] GameObject _minis;
-    [SerializeField] GameObject _minisArea;
+    [SerializeField] private GameObject _minis;
+    [SerializeField] private GameObject _minisArea;
+    [SerializeField] private GameObject _building;
+    [SerializeField] private Sprite _nextBuildingTier;
+    [SerializeField] private ParticleSystem _particleSystem;
 
     public void ConstructionCompleted()
     {
+        _particleSystem.Play();
+        _building.GetComponent<SpriteRenderer>().sprite = _nextBuildingTier;
         _minis.SetActive(true);
         _minisArea.SetActive(true);
     }
